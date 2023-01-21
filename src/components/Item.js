89ee59bf623f4ae385d0503dcc382dc0c1ae5219
Liza McLain <lizamclain@gmail.com>
2023-01-21 16:19:46 -0600

    commit tree f3db299ca77ad5261aa9b4b21b8e54517feba33b

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 function Item({ item }) {
+  function handleAddToCartClick() {
+    console.log("clicked item:", item);
+  }
+  
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button onClick={handleAddToCartClick} className={item.isInCart ? "remove" : "add"}>
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
       <button className="remove">Delete</button>
